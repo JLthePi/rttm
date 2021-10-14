3e3e8b5f63a13b1b3e57d9ab37129298f0325885
JLthePi <jlthepi@gmail.com>
2021-10-14 19:27:02 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Edit <code>src/App.js</code> and save to reload.
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <div>
+          <Link to="/">
+            Home
+          </Link>
+          <Link to="/about">
+            About
+          </Link>
+          <Link to="/contact">
+            Contact
+          </Link>
+        </div>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <div>This is Home</div>
+            </Route>
+            <Route path="/about">
+              <div>This is About</div>
+            </Route>
+            <Route path="/contact">
+              <div>This is Contact</div>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
